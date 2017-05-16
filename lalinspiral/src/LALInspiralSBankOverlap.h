894882f6e10fbb392acf894482948b2e2a511eb5
Ian Harry <ian.harry@ligo.org>
2017-05-16 09:54:12 +0200

    COMPLEX8 -> COMPLEX16 (almost complete)

--- lalinspiral/src/LALInspiralSBankOverlap.h
+++ lalinspiral/src/LALInspiralSBankOverlap.h
@@ -6,17 +6,17 @@
 
 typedef struct tagWS {
     size_t n;
-    COMPLEX8FFTPlan *plan;
-    COMPLEX8Vector *zf;
-    COMPLEX8Vector *zt;
+    COMPLEX16FFTPlan *plan;
+    COMPLEX16Vector *zf;
+    COMPLEX16Vector *zt;
 } WS;
 
 WS *XLALCreateSBankWorkspaceCache(void);
 void XLALDestroySBankWorkspaceCache(WS *workspace_cache);
-REAL8 XLALInspiralSBankComputeMatch(const COMPLEX8FrequencySeries *inj, const COMPLEX8FrequencySeries *tmplt, WS *workspace_cache);
+REAL8 XLALInspiralSBankComputeMatch(const COMPLEX16FrequencySeries *inj, const COMPLEX16FrequencySeries *tmplt, WS *workspace_cache);
 
-REAL8 XLALInspiralSBankComputeRealMatch(const COMPLEX8FrequencySeries *inj, const COMPLEX8FrequencySeries *tmplt, WS *workspace_cache);
+REAL8 XLALInspiralSBankComputeRealMatch(const COMPLEX16FrequencySeries *inj, const COMPLEX16FrequencySeries *tmplt, WS *workspace_cache);
 
-REAL8 XLALInspiralSBankComputeMatchMaxSkyLoc(const COMPLEX8FrequencySeries *hp, const COMPLEX8FrequencySeries *hc, const REAL8 hphccorr, const COMPLEX8FrequencySeries *proposal, WS *workspace_cache1, WS *workspace_cache2);
+REAL8 XLALInspiralSBankComputeMatchMaxSkyLoc(const COMPLEX16FrequencySeries *hp, const COMPLEX16FrequencySeries *hc, const REAL8 hphccorr, const COMPLEX16FrequencySeries *proposal, WS *workspace_cache1, WS *workspace_cache2);
 
-REAL8 XLALInspiralSBankComputeMatchMaxSkyLocNoPhase(const COMPLEX8FrequencySeries *hp, const COMPLEX8FrequencySeries *hc, const REAL8 hphccorr, const COMPLEX8FrequencySeries *proposal, WS *workspace_cache1, WS *workspace_cache2);
+REAL8 XLALInspiralSBankComputeMatchMaxSkyLocNoPhase(const COMPLEX16FrequencySeries *hp, const COMPLEX16FrequencySeries *hc, const REAL8 hphccorr, const COMPLEX16FrequencySeries *proposal, WS *workspace_cache1, WS *workspace_cache2);
